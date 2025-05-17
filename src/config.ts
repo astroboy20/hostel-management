655c8c23c734de70f85d2ad0b9832db32822a656
astroboy20 <tolulopeakinkunmi7@gmail.com>
2025-05-17 19:19:18 +0100

    first commit

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+interface EnvVariables {
+    PORT: number
+    MONGODB_URI: string
+    JWT_SECRET: string
+    PAYSTACK_SECRET_KEY: string;
+}
+
+export const config: EnvVariables = {
+    PORT: parseInt(process.env.PORT || '5000'),
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/hostel_db',
+    JWT_SECRET: process.env.JWT_SECRET || 'secret',
+    PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY || '',
+}
